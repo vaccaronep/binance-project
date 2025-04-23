73a62c6567cc4acf43dcd9a557107694f6a73ba5
vaccaronep <37909628+vaccaronep@users.noreply.github.com>
2025-04-22 21:41:00 -0300

    add ws market prices based on wishlist (#12)
    Co-authored-by: Patricio Vaccarone <patricio.vaccarone@ge.com>

--- orders-microservice/src/services/orders.service.ts
+++ orders-microservice/src/services/orders.service.ts
@@ -169,6 +169,7 @@ export class OrdersService {
         stopPrice,
         stopLimitPrice,
         stopLimitTimeInForce: 'GTC',
+        strategyId: config._id,
         stopStrategyId: order.strategyId.toString(),
         limitStrategyId: order.strategyId.toString(),
       };
